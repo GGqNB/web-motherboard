9761d07faba541a6fd4fcf29389e37460eccd15c
Андрей Билялов <andreyka.drogan@gmail.com>
2023-12-26 22:19:10 +0500

    fix network

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,10 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as dotenv from 'dotenv';
-
-// export const BASE_SERVER_API_URL = "http://127.0.0.1:8000";
-// export const URL_BASE_SERVER: string = process.env.SERVER_BASE_URL as string;
-// export const STOWN_URL_BASE_SERVER: string = process.env.STOWN_BASE_URL as string;
-// export const DADATA_KEY: string = process.env.DADATA_API_KEY as string;
-// export const API_SERVER: string = `${URL_BASE_SERVER}/api`;
-// export const STOWN_API_SERVER: string = `${STOWN_URL_BASE_SERVER}/api`;
-export const API_SERVER: string = process.env.SERVER_URL;
+//for deploy
+export const API_SERVER: string = window.location.origin+'/api';
+// export const API_SERVER: string = process.env.SERVER_URL;
